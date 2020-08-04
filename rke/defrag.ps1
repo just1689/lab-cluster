0443f03d26afb24216dec69e8a164ba0387b0499
Just1689 <Just1689@gmail.com>
2020-08-04 23:02:31 +0200

    Compress Windows Hyper-V disks

---
+++ rke/defrag.ps1
@@ -0,0 +1,9 @@
+Optimize-VHD -Path "D:\Vms\registry\Virtual Hard Disks\registry.vhdx" -Mode Full
+Optimize-VHD -Path "D:\Vms\rke-1\Virtual Hard Disks\rke-1.vhdx" -Mode Full
+Optimize-VHD -Path "D:\Vms\rke-2\Virtual Hard Disks\rke-2.vhdx" -Mode Full
+Optimize-VHD -Path "D:\Vms\rke-3\Virtual Hard Disks\rke-3.vhdx" -Mode Full
+Optimize-VHD -Path "D:\Vms\rke-4\Virtual Hard Disks\rke-4.vhdx" -Mode Full
+Optimize-VHD -Path "D:\Vms\rke-5\Virtual Hard Disks\rke-5.vhdx" -Mode Full
+
+
+
